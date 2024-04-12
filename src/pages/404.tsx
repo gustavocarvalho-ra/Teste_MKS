3e317960e417cd2eca825174913052e6780e5867
GustavoCR021 <gustavoc1922@gmail.com>
2024-04-12 14:53:29 -0300

    feat: 404 page styling

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,21 @@
-import Link from "next/link";
+// import Link from "next/link";
+import Image from "next/image";
 
 export default function NotFound() {
   return (
-    <main>
-      <p>not found page</p>
-        <Link
+    <main style={{width: '100%', height: '100vh',display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', background: '#030813'}}>
+      <Image
+          src="/404.svg"
+          alt="Imagem do carrinho de compra"
+          width={750}
+          height={750}
+        />
+        {/* <Link
+          color="red"
           href=" /">
           Go back home
-        </Link>
+        </Link> */}
+        <a href=" /" style={{color: '#fff', fontSize: '30px', fontFamily: 'Montserrat'}}>Clique aqui para voltar</a>
     </main>
   );
 }
